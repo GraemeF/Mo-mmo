5bc98f12f3741fc5a8b499fc4071c7efde5b9326
Graeme Foster <graemef@gmail.com>
2011-11-12 19:12:30 +0000

    Build when first run.

--- Continuous-Test.ps1
+++ Continuous-Test.ps1
@@ -1,7 +1,15 @@
 Import-Module pswatch
 
+Function Rebuild($message)
+{
+	cls
+	Write-Host $message
+	Invoke-psake .\build.ps1
+}
+
+Rebuild "Starting up."
 watch | Where-Object { $_.Path -notlike "*.git*" } | Get-Item | Where-Object { $_.Extension -eq ".coffee" } | %{
 	cls
 	Write-Host "$_ changed."
-	Invoke-psake -taskList test .\build.ps1
+	Invoke-psake .\build.ps1
 }
