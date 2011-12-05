10943a5385bbcf8e0ea4a62d871af30443c08a25
Graeme Foster <graemef@gmail.com>
2011-12-05 21:54:42 +0000

    Run Jake continuously.

--- Continuous-Test.ps1
+++ Continuous-Test.ps1
@@ -11,5 +11,5 @@ Rebuild "Starting up."
 watch | Where-Object { ($_.Path -notlike "*.git*") -and ($_.Path -notlike "*.idea*") } | %{ Write-Output $_.Path.Replace("___jb_bak___", "") } | Where-Object { $_.EndsWith(".coffee") } | %{
 	cls
 	Write-Host "$_ changed."
-	Invoke-psake .\build.ps1
+	jake
 }
