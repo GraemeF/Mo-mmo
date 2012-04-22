8696e6ee43715aa188223491897b3b44fd936b2f
Graeme Foster <graemef@gmail.com>
2012-04-22 17:45:01 +0100

    Simplified server startup.

--- features/support/server-hooks.js
+++ features/support/server-hooks.js
@@ -2,11 +2,9 @@ var spawn = require('child_process').spawn;
 var zombie = require("zombie");
 
 var runServer = function (callback) {
-    var commandProcess;
-    commandProcess = spawn("node", ["lib/server"]);
+    var commandProcess = spawn("node", ["lib/server"]);
     commandProcess.stdout.on('data', function (data) {
         if (data == 'Ready.\n') {
-            console.log("Started", commandProcess);
             callback(null, commandProcess);
         }
     });
@@ -24,28 +22,20 @@ var runServer = function (callback) {
 
 hooks = function () {
     this.Around(function (runScenario) {
-        console.log("Adding hooks around a scenario");
         var world = this;
 
         runServer(function (error, serverProcess) {
             world.serverProcess = serverProcess;
-
-            console.log('Creating zombie.');
             world.zombie = new zombie.Browser({
                 runScripts:true,
-                site:"http://localhost:3003",
                 debug:true,
                 userAgent:"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.59 Safari/535.7"
             });
-            zombie.visit('/index.html', function (err, newBrowser, status) {
-                console.log('Running scenario.');
+
+            zombie.visit('http://localhost:3003/index.html', function (err, newBrowser) {
                 world.zombie = newBrowser;
-
                 runScenario(function (callback) {
-                    console.log('Scenario finished, cleaning up.');
-
                     serverProcess.kill();
-
                     callback();
                 });
             });
