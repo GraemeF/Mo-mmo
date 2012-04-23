36f416e717266cda43ec62e4d3924d15a23a015f
Graeme Foster <graemef@gmail.com>
2012-04-23 19:42:01 +0100

    Use the patience package.

--- features/step_definitions/characterSteps.js
+++ features/step_definitions/characterSteps.js
@@ -1,37 +1,5 @@
 require('chai').should();
-
-var soon = function (test, context, callback) {
-    var attempts = 0;
-
-    var seeIfTestPasses = function (test, context, callback) {
-        try {
-            test.call(context);
-            callback();
-        }
-        catch (e) {
-            callback(e);
-        }
-    };
-
-    var retryingCallback = function (error) {
-        if (error) {
-            if (attempts++ < 100) {
-                setTimeout(function () {
-                    seeIfTestPasses(test, context, retryingCallback);
-                }, 10);
-            }
-            else {
-                callback.fail(error);
-            }
-        }
-        else
-            callback();
-    };
-
-    process.nextTick(function () {
-        seeIfTestPasses(test, context, retryingCallback);
-    });
-};
+var soon = require('patience').soon;
 
 module.exports = function () {
 
